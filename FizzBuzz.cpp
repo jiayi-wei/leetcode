fa9a5342fbc88681a5f246f0e737ebff644a5acb
jiayi-wei <weijiayi1217@gmail.com>
2016-10-10 23:41:03 +0800

    Add files via upload

---
+++ FizzBuzz.cpp
@@ -0,0 +1,37 @@
+#include<string>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+vector<string> fizzBuzz(int n) {
+	vector<string> result;
+	for (int i = 1; i<n + 1; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			result.push_back("FizzBuzz");
+		else if (i % 3 == 0)
+			result.push_back("Fizz");
+		else if (i % 5 == 0)
+			result.push_back("Buzz");
+		else
+		{
+			int count = i;
+			string s= "";
+			do{
+				int digit = count % 10;
+				s.insert(s.begin(), char('0' + digit));
+				count = count / 10;
+			} while (count > 0);
+			result.push_back(string(s));
+		}
+	}
+	return result;
+}
+
+void main()
+{
+	int n = 15;
+	vector<string> sc = fizzBuzz(n);
+	system("pause");
+}
